32a7b619004ee757f47f6f4a7c8b8b81ca11fdde
GMCadiom <gm.cadiom@gmail.com>
2017-03-27 16:29:12 +0000

    Tools : Add toolchain To Local Work

---
+++ BuildKernel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Mysteryagr
+#Compile kernel with a build script to make things simple
+
+mkdir -p out
+
+export CROSS_COMPILE=$PWD/linaro/bin/arm-eabi-
+
+#Change toolchain path before using build script!
+#export CROSS_COMPILE=~/toolchains/arm-eabi-linaro-4.7.3/bin/arm-eabi-
+#Using toolchain in Travis
+
+#Enable when needed:
+export USE_CCACHE=1
+
+export ARCH=arm ARCH_MTK_PLATFORM=mt6582
+
+#Enable only when needed:
+#make clean
+#make mrproper
+#Or simply delete out directory to clean source
+
+make -C $PWD O=$PWD/out ARCH=arm sprout_defconfig
+
+#Edit the number according to the number of CPUs you have in your PC:
+make -j4 -C $PWD O=$PWD/out ARCH=arm

f05cb3b73966f01720d1bd7f220b4e5498fef7c9
Rendy Arya Kemal <rendyarya22@gmail.com>
2017-09-10 07:47:58 +0700

    Selinuxfs.c: Add #endif to end #ifdef CONFIG_ALWAYS_ENFORCE

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -196,6 +196,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 		selnl_notify_setenforce(selinux_enforcing);
 		selinux_status_update_setenforce(selinux_enforcing);
 	}
+#endif
 	length = count;
 out:
 	free_page((unsigned long) page);
